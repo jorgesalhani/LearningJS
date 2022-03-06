b3ed722cd335f25dc0e43b97b520ea314e2dce49
Jorge Salhani <jorge.salhani@qitech.com.r>
2022-03-06 15:14:46 -0300

    First sketch in p5 js

---
+++ BouncingBall/sketch.js
@@ -0,0 +1,22 @@
+var x = 50;
+var speed = 3;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  stroke(255);
+  strokeWeight(4);
+  noFill();
+  ellipse(x, 200, 100, 100);
+  
+
+  if (x+50 > width || x-50 < 0) {
+      speed = speed * (-1);
+  }
+
+  x += speed;
+
+}
