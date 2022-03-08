370e778d48b272275030238e2b4635f77464c62b
Jorge Salhani <jorge.salhani@qitech.com.r>
2022-03-07 23:31:46 -0300

    let... of...

--- DrawBubblesArray/sketch.js
+++ DrawBubblesArray/sketch.js
@@ -12,6 +12,11 @@ function setup() {
 
 function draw() {
   background(0);
+
+  // for (let bubble of bubbles) {
+  //   bubble.move();
+  //   bubble.show();
+  // }
   
   for (let i = 0; i < bubbles.length; i++) {
     bubbles[i].move();
