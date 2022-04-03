ace756cafb0c53d89a51c789532ab55d601d702c
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-04-03 12:39:59 -0300

    Click event

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,64 +1,13 @@
-let bubbles = [];
+document.getElementById('button').click();
 
-function setup() {
-  createCanvas(400, 400);
-  for (let i = 0; i < 50; i++) {
-    let x = random(width);
-    let y = random(height);
-    let r = random(10, 40);
-    bubbles[i] = new Bubble(x, y, r);
-  }
-}
+const body = document.body;
 
-function mousePressed() {
-  for (let i = 0; i < 50; i++) {
-    bubbles[i].clicked(mouseX, mouseY);
-  }
-}
+body.addEventListener('click', e => {
+  console.log('clicked body');
+});
 
-function draw() {
-  background(0);
+console.log('Using click()');
+body.click();
 
-  // for (let bubble of bubbles) {
-  //   bubble.move();
-  //   bubble.show();
-  // }
-  
-  for (let i = 0; i < bubbles.length; i++) {
-    bubbles[i].move();
-    bubbles[i].show();
-  }
-}
-
-// function mousePressed() {
-//   let r = random(10, 50);
-//   let b = new Bubble(mouseX, mouseY, r);
-//   bubbles.push(b);
-// }
-
-class Bubble {
-  constructor(x, y, r) {
-    this.x = x,
-    this.y = y,
-    this.r = r,
-    this.brightness = 100
-  }
-
-  clicked(mx, my) {
-    let d = dist(mx, my, this.x, this.y);
-    if (d < this.r) {
-      this.brightness = 255
-    }
-  }
-
-  move() {
-    this.x = this.x + random(-2, 2);
-    this.y = this.y + random(-2, 2);
-  }
-
-  show() {
-    noStroke()
-    fill(this.brightness, 100);
-    ellipse(this.x, this.y, this.r * 2);
-  }
-}
+console.log('Using dispatchEvent');
+body.dispatchEvent(new Event('click'));
