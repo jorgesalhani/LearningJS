bdf46ddf1614223a711c8af77f1b3d9b96692f37
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-04-03 16:28:24 -0300

    random number grahp

--- docs/sketch.js
+++ docs/sketch.js
@@ -1,29 +1,91 @@
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
+let data_labels = [0,1,2,3,4,5]
 
-const data = {
-  labels: labels,
+let data_array = [1];
+
+let data = {
+  labels: data_labels,
   datasets: [{
     label: 'My First dataset',
     backgroundColor: 'rgb(255, 99, 132)',
     borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
+    data: data_array,
   }]
 };
 
-const config = {
+let config = {
   type: 'line',
   data: data,
   options: {}
 };
 
-const myChart = new Chart(
+let myChart = new Chart(
   document.getElementById('myChart'),
   config
 );
+
+function dataUpdate(data, data_array, data_labels) {
+  data.labels = data_labels;
+  data.datasets[0].data = data_array;
+  myChart.destroy();
+  data = {
+    labels: data_labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: data_array,
+    }]
+  };
+  
+  config = {
+    type: 'line',
+    data: data,
+    options: {
+      animation: {
+        duration: 0
+      }
+    }
+  };
+  
+  myChart = new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+  
+  return myChart;
+}
+
+function insertData(data_array, data_labels) {
+  let labels_length = data_labels.length
+  let data_length = data_array.length
+
+  let next = Math.random()*50;
+
+  data_array.push(next);
+  if (data_length > 5) {
+    data_labels.push(labels_length);
+  }
+}
+
+const insertDataButton = document.getElementById('isertDataButton');
+insertDataButton.addEventListener('click', function(){
+  insertData(data_array, data_labels)
+  dataUpdate(data, data_array, data_labels);
+});
+
+function performSimulation(data_array, data_labels, simulation_end) {
+  if (simulation_end <= 1) {
+    return;
+  }
+  insertData(data_array, data_labels);
+  setTimeout(function() {
+    performSimulation(data_array, data_labels, simulation_end);
+    dataUpdate(data, data_array, data_labels)
+  }, 2*simulation_end);
+  simulation_end--;
+}
+
+const initSimulation = document.getElementById('initSimulationButton');
+initSimulation.addEventListener('click', function() {
+  performSimulation(data_array, data_labels, simulation_end=100);
+});
