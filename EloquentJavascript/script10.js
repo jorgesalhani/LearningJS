a5efde087e1d926a8229011bd66b92ffaa959edc
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-05-29 18:41:11 -0300

    Finish chap 10 - Modules (CommonJS, ES modules, bundlers, minifiers)

---
+++ EloquentJavascript/script10.js
@@ -0,0 +1,23 @@
+const weekDay = function() {
+  const names = ["Sunday", "Monday", "Tuesday", "Wednesday",
+  "Thursday", "Friday", "Saturday"];
+
+  return {
+    name(number) {return names[number];},
+    number(name) {return names.indexOf(name);}
+  };
+}();
+
+// console.log(weekDay.number('Thursday'));
+
+const x = 1;
+function evalAndReturnX(code) {
+  eval(code);
+  return x;
+}
+
+// console.log(evalAndReturnX('var x = 2')); // 2
+// console.log(x); // 1
+
+let plusOne = Function('n', 'return n + 1;');
+// console.log(plusOne(5)); // 6
