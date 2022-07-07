cf15eedcec720180849efb38bb99555b39b7cb86
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-07-07 14:00:25 -0300

    Collision Box working

--- docs/CollisionBox/script.js
+++ docs/CollisionBox/script.js
@@ -4,6 +4,8 @@ function pixelsToFloat(pixelsMetric) {
 }
 
 let width = pixelsToFloat(d3.select('svg').style('width'));
+let heigth = pixelsToFloat(d3.select('svg').style('height'));
+
 
 let svg = d3.select('svg g');
 
@@ -15,23 +17,77 @@ svg
   .attr('fill', 'white')
   .attr('stroke', 'black');
 
+let r = 10
 svg
   .append('circle')
-  .attr('r', 10)
+  .attr('r', r)
   .attr('cx', width / 4)
   .attr('cy', 100);
 
 
 let circle = d3.select('circle');
 
-function moveCircle(circle, vx, vy) {
-  let currentPositionX = pixelsToFloat(circle.style('cx'));
-  circle.attr('cx', currentPositionX + vx);
+let circlePositions = [{
+  x: pixelsToFloat(circle.style('cx')),
+  y: pixelsToFloat(circle.style('cy'))
+}]
 
-  let currentPositionY = pixelsToFloat(circle.style('cy'));
-  circle.attr('cy', currentPositionY + vy);
-
-  circle
-    .transition()
-    .duration(500); 
+function updatePositions() {
+  circlePositions = [];
+  circle = d3.selectAll('circle');
+  circlePositions.push({
+    x: pixelsToFloat(circle.style('cx')),
+    y: pixelsToFloat(circle.style('cy'))
+  })
 }
+
+console.log(width);
+console.log(heigth);
+
+let dt = 1;
+let vx = 2, vy = 2;
+
+function horizontalWallCollision(currentX, currentY) {
+  let nextPositionX = currentX + vx * dt;
+  let nextPositionY = currentY + vy * dt;
+  if (nextPositionX <= r || nextPositionX >= (width / 2) - r) {
+    let incidentAngle = Math.atan2(nextPositionX, nextPositionY);
+    vx *= -1;
+    nextPositionX =  currentX + (vx * dt) * Math.cos(incidentAngle);
+  }
+  return nextPositionX;
+}
+
+function verticalWallCollision(currentX, currentY) {
+  let nextPositionX = currentX + vx * dt;
+  let nextPositionY = currentY + vy * dt;
+  if (nextPositionY <= r || nextPositionY >= (heigth / 2) - r) {
+    let incidentAngle = Math.atan2(nextPositionX, nextPositionY);
+    vy *= -1;
+    nextPositionY = currentY + (vy * dt)* Math.sin(incidentAngle);
+  }
+  return nextPositionY;
+}
+
+function moveCircle() {
+  d3.selectAll('circle')
+    .data(circlePositions)
+    .join('circle')
+    .attr('cx', function(d, i) {
+      nextPositionX = horizontalWallCollision(d.x, d.y);
+      return nextPositionX;
+    })
+    .attr('cy', function(d, i) {
+      nextPositionY = verticalWallCollision(d.x, d.y);
+      return nextPositionY;
+    });
+}
+
+let interval = d3.interval(function(elapsed) {
+  // if (elapsed > 10000) {
+  //   interval.stop();
+  //   return;
+  // }
+  updatePositions();
+  moveCircle();
+}, 1);
