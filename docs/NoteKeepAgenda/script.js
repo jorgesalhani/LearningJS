ab5fae72384bcf1feb9208df98023eed57140f7d
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-07-20 18:34:15 -0300

    Start table

--- docs/NoteKeepAgenda/script.js
+++ docs/NoteKeepAgenda/script.js
@@ -1,13 +1,37 @@
 const form = document.getElementById('note-form');
+let noteDB = [];
 
-function sendData() {  
-  for (let i = 0; i < form.length; i++) {
-    
-    console.log(i);
+function sendData() {
+  let formJson = {
+    "description": form.elements[2].value,
+    "todo": form.elements[0].value,
+    "deadline": form.elements[1].value
+  };
+  noteDB.push(formJson);
+}
+
+function createTable() {
+  newTable = document.createElement('table');
+  newTable.style.width = '100px';
+  newTable.style.border = '1px solid black';
+
+  document.body.appendChild(newTable);
+  let formKeys = Object.keys(noteDB[0]);
+  formKeys.forEach(element => {
+    let row = newTable.insertRow();
+    console.log(element);
+  });
+}
+
+function updateTable() {
+  let newTable = document.getElementById('note-table');
+  if (newTable !== undefined) {
+    createTable();
   }
 }
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   sendData();
+  updateTable();
 });
