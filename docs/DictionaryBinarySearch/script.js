6950cc02b5af1d0764f15363f1ab85e38e451c94
jorge.salhani@usp.br <jorge.salhani@usp.br>
2022-07-24 16:04:41 -0300

    Get content text from wikipedia page

--- docs/DictionaryBinarySearch/script.js
+++ docs/DictionaryBinarySearch/script.js
@@ -4,28 +4,63 @@ let form = document.querySelector('form');
 
 async function searchDefinition() {
   let word = form.querySelector('#word').value;
-  // Using API from URL: https://api.dicionario-aberto.net/index.html
-  let url = new URL(`https://en.wikipedia.org`);
-  xhr.open('GET', url, true);
-  xhr.send();
-  let resp = new Map();
-  xhr.onload = function() {
-    // console.log(`Loaded: ${xhr.status} ${xhr.response}`);
-    resp.set('status', xhr.status);
-    resp.set('response', xhr.response);
+  let url = new URL(`https://pt.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=5&gsrsearch='${word}'`);
+  let respJSON = {};
+  const resp = await fetch(url, {
+    method: 'GET',
+    headers: {}
+  });
+
+  if (!resp.ok) {
+    alert('Oops! Network error');
   }
-  xhr.onerror = function() {
-    alert('Network error');
+
+  try {
+    respJSON = resp.json();
+  } catch {
+    alert('Oops! JSON parse error');
   }
-  return resp;
+
+  return respJSON;
 }
 
-function displayDefinition(definition) {
-  console.log(definition);
+async function getPageContent(wikiResponseJSON) {
+  let pagesObject = wikiResponseJSON.query.pages;
+  let firstTitle = Object.values(pagesObject)[0]['title']
+  let url = new URL(`http://pt.wikipedia.org/w/api.php?action=parse&origin=*&prop=text&page=${firstTitle}&format=json`)
+  let respJSON = {};
+  const resp = await fetch(url, {
+    methdo: 'GET',
+    headers: {}
+  });
+
+  try {
+    respJSON = resp.json();
+  } catch {
+    console.error('Oops! JSON parse error');
+  }
+
+  return respJSON;
+}
+
+async function getPageText(pageResponse) {
+  let pageText = '';
+  try {
+    pageText = pageResponse['parse']['text'];
+  } catch {
+    console.error('Oops! Get page text error');
+  }
+  return pageText;
+}
+
+async function getFirstParagraph(pageText) {
+  console.log(pageText);
 }
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
-  let definition = await searchDefinition();
-  displayDefinition(definition);
+  let wikiResponseJSON = await searchDefinition();
+  let firstPageResponse = await getPageContent(wikiResponseJSON);
+  let pageText = await getPageText(firstPageResponse);
+  await getFirstParagraph(pageText['*']);
 })
